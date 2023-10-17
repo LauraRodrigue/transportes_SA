630c4d4910a3408b9c9644c7a4dcb90d5afc57cf
Karol <124207015+Karollll@users.noreply.github.com>
2023-10-17 17:54:19 -0500

    laura

--- transporte/src/App.vue
+++ transporte/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  
   <div>
     <router-link to="/"><q-btn color="white" text-color="black" label="login" /></router-link>
     <router-link to="/buses"><q-btn color="white" text-color="black" label="buses" /></router-link>
--- transporte/src/components/login.vue
+++ transporte/src/components/login.vue
@@ -1,10 +1,41 @@
 <template>
-  
+    <div class="column items-center">
+
+    <q-card class="my-card bg-amber-4 text-black"  style="height: 300px; width: 45%;">
+      <q-card-section>
+       <q-form @submit="onSubmit" @reset="onReset">
+          <q-input
+            filled
+            v-model="name"
+            label="Nombre"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please type something',
+            ]"
+          />
+
+          <q-input
+            filled
+            type="number"
+            v-model="age"
+            label="Contraseña"
+            lazy-rules
+            :rules="[
+              (val) => (val > 0 && val < 100) || 'Please type a real age',
+            ]"
+          />
+
+          <q-toggle v-model="accept" label="I accept the license and terms" />
+
+          <div class="text-center">
+            <q-btn label="Ingresar" type="submit" color="teal-5" />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+    </div>
+
 </template>
-<script setup>
+<script setup></script>
 
-</script>
 
-<style scoped>
-
-</style>
