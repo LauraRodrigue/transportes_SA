ba941c7e60949a998e2ebea94346fb3ef61d8f03
Karol <124207015+Karollll@users.noreply.github.com>
2023-10-17 17:59:23 -0500

    que estres

--- transporte/src/App.vue
+++ transporte/src/App.vue
@@ -1,37 +1,9 @@
 <template>
+  
   <div>
     <router-link to="/"><q-btn color="white" text-color="black" label="login" /></router-link>
     <router-link to="/buses"><q-btn color="white" text-color="black" label="buses" /></router-link>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-  </q-layout>
-
-  <router-view></router-view>
+    <router-view></router-view>
+  </div>
+  
 </template>
-
-<<<<<<< HEAD
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-}
-</script>
-=======
-
-
-
->>>>>>> 3ba9892e64943fced9161d037a243d5b04cd67a6
