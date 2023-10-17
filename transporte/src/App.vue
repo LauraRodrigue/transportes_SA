3318b217f815c7766956aaba7a5a5e88dc6dc1b7
lauraRodrigue <>
2023-10-17 16:02:51 -0500

    hola

--- transporte/src/App.vue
+++ transporte/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <router-link to="/"><q-btn color="white" text-color="black" label="login" /></router-link>
-    <router-link to="/"><q-btn color="white" text-color="black" label="buses" /></router-link>
+    <router-link to="/buses"><q-btn color="white" text-color="black" label="buses" /></router-link>
     <router-view></router-view>
   </div>
   
