fa6870035a850abfb6282430e32a361ec3f7a3bd
lauraRodrigue <>
2023-10-17 14:31:56 -0500

    hola

--- transporte/src/main.js
+++ transporte/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import{router} from "./routers/routes.js"
+import{router} from "./routers/routers.js"
 import { Quasar } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
