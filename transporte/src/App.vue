1bf7468609c1e4e0d422736b9ef2e5c9ed4b785a
lauraRodrigue <>
2023-10-17 17:32:27 -0500

    hola

--- transporte/src/App.vue
+++ transporte/src/App.vue
@@ -1,8 +1,46 @@
 <template>
-  <div>
-    <router-link to="/"><q-btn color="white" text-color="black" label="login" /></router-link>
+  <q-layout class="uno" view="hHh lpR lFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" behavior="mobile" bordered>
+      <router-link to="/"><q-btn color="white" text-color="black" label="login" /></router-link>
     <router-link to="/buses"><q-btn color="white" text-color="black" label="buses" /></router-link>
-    <router-view></router-view>
-  </div>
-  
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+
+  <router-view></router-view>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
