4ebe6ed1550b79b6f7c14b6abef95d00e14335f4
lauraRodrigue <>
2023-10-18 16:23:40 -0500

    laura

--- transporte/src/components/menu.vue
+++ transporte/src/components/menu.vue
@@ -28,28 +28,98 @@
 
                     <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
                         icon="directions" />
+                    <router-link to="/billete"><q-btn text-color="black" label="boletos" /></router-link><br>
+
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
                     <router-link to="/buses"><q-btn text-color="black" label="buses" /></router-link><br>
 
                     <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
                         icon="directions" />
-                        <router-link to="/cliente"><q-btn text-color="black" label="Cliente" /></router-link><br>
+                    <router-link to="/cliente"><q-btn text-color="black" label="clientes" /></router-link><br>
+                
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                    <router-link to="/conductor"><q-btn text-color="black" label="conductores" /></router-link><br>
 
                     <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
                         icon="directions" />
+                    <router-link to="/horario"><q-btn text-color="black" label="horarios" /></router-link><br>
+
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                    <router-link to="/ruta"><q-btn text-color="black" label="rutas" /></router-link><br>
+
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                    <router-link to="/vendedor"><q-btn text-color="black" label="vendedores" /></router-link><br>
                 </div>
 
             </q-drawer>
 
             <q-page-container>
-                
+                <div class="row">
+                    <div>
+                        <q-card class="my-card bg-purple text-white">
+                            <q-card-section>
+                                <div class="text-h6">Our Changing Planet</div>
+                                <div class="text-subtitle2">by John Doe</div>
+                            </q-card-section>
+
+                            <q-card-actions>
+                                <q-btn flat>Action 1</q-btn>
+                                <q-btn flat>Action 2</q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div>
+                        <q-card class="my-card bg-purple text-white">
+                            <q-card-section>
+                                <div class="text-h6">Our Changing Planet</div>
+                                <div class="text-subtitle2">by John Doe</div>
+                            </q-card-section>
+
+                            <q-card-actions>
+                                <q-btn flat>Action 1</q-btn>
+                                <q-btn flat>Action 2</q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div>
+                        <q-card class="my-card bg-purple text-white">
+                            <q-card-section>
+                                <div class="text-h6">Our Changing Planet</div>
+                                <div class="text-subtitle2">by John Doe</div>
+                            </q-card-section>
+
+                            <q-card-actions>
+                                <q-btn flat>Action 1</q-btn>
+                                <q-btn flat>Action 2</q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div>
+                        <q-card class="my-card bg-purple text-white">
+                            <q-card-section>
+                                <div class="text-h6">Our Changing Planet</div>
+                                <div class="text-subtitle2">by John Doe</div>
+                            </q-card-section>
+
+                            <q-card-actions>
+                                <q-btn flat>Action 1</q-btn>
+                                <q-btn flat>Action 2</q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                </div>
                 <router-view />
             </q-page-container>
 
         </q-layout>
 
-        
+
         <div class="menus">
-            
+
         </div>
 
     </div>
--- transporte/src/routes/routes.js
+++ transporte/src/routes/routes.js
@@ -3,12 +3,22 @@ import login from '../components/login.vue';
 import buses from '../components/buses.vue';
 import menu from '../components/menu.vue';
 import cliente from '../components/cliente.vue';
+import billete from '../components/billete.vue';
+import conductor from '../components/conductor.vue';
+import horario from '../components/horario.vue';
+import ruta from '../components/ruta.vue';
+import vendedor from '../components/vendedor.vue';
 
 const routes = [
     { path: '/', component: login },
     { path: '/menu', component: menu, children:[
        { path: '/buses', component: buses },
-       { path: '/cliente', component: cliente}
+       { path: '/cliente', component: cliente},
+       { path: '/billete', component: billete},
+       { path: '/conductor', component: conductor},
+       { path: '/horario', component: horario},
+       { path: '/ruta', component: ruta},
+       { path: '/vendedor', component: vendedor}
     ] },
     
 ];
