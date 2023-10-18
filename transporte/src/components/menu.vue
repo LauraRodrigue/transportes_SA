9113978606f7c764ce4db2f9e87c674a1ac4ff60
lauraRodrigue <>
2023-10-18 15:44:43 -0500

    laura

--- transporte/src/components/menu.vue
+++ transporte/src/components/menu.vue
@@ -1,52 +1,74 @@
 <template>
-    <q-layout class="bg-orange-1" view="hHh lpR lFf">
-  
-      <q-header elevated class="bg-orange-10 text-white">
-        <q-toolbar>
-          <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-  
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            Transportes S.A
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-  
-      <q-drawer v-model="leftDrawerOpen" side="left" behavior="mobile" bordered>
-        <div class="q-pa-md q-gutter-sm">
-      <q-avatar>
-        <img src="https://cdn.quasar.dev/img/avatar.png">
-      </q-avatar>
+    <div class="contenedor">
+        <q-layout class="bg-orange-1" view="hHh lpR lFf">
+
+            <q-header elevated class="bg-orange-10 text-white">
+                <q-toolbar>
+                    <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+                    <q-toolbar-title>
+                        <q-avatar>
+                            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+                        </q-avatar>
+                        Transportes S.A.S
+                    </q-toolbar-title>
+                </q-toolbar>
+            </q-header>
+
+            <q-drawer class="bg-green-1" v-model="leftDrawerOpen" side="left" behavior="mobile" bordered>
+                <div class="q-pa-md q-gutter-sm">
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/img/avatar.png">
+                    </q-avatar>
+                </div>
+                <div class="q-pa-md q-gutter-sm">
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                    <router-link to="/menu"><q-btn text-color="black" label="Menu" /></router-link><br>
+
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                    <router-link to="/buses"><q-btn text-color="black" label="buses" /></router-link><br>
+
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                        <router-link to="/cliente"><q-btn text-color="black" label="Cliente" /></router-link><br>
+
+                    <q-avatar v-for="size in ['md']" :key="size" :size="size" color="primary" text-color="white"
+                        icon="directions" />
+                </div>
+
+            </q-drawer>
+
+            <q-page-container>
+                
+                <router-view />
+            </q-page-container>
+
+        </q-layout>
+
+        
+        <div class="menus">
+            
+        </div>
+
     </div>
-      <router-link to="/"><q-btn color="white" text-color="black" label="login" /></router-link><br>
-      <router-link to="/buses"><q-btn color="white" text-color="black" label="buses" /></router-link>
-      </q-drawer>
+</template>
   
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-  
-    </q-layout>
-  
-    <router-view></router-view>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'
-  
-  export default {
-    setup () {
-      const leftDrawerOpen = ref(false)
-  
-      return {
-        leftDrawerOpen,
-        toggleLeftDrawer () {
-          leftDrawerOpen.value = !leftDrawerOpen.value
+<script>
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const leftDrawerOpen = ref(false)
+
+        return {
+            leftDrawerOpen,
+            toggleLeftDrawer() {
+                leftDrawerOpen.value = !leftDrawerOpen.value
+            }
         }
-      }
     }
-  }
-  </script>
+}
+</script>
   
--- transporte/src/routes/routes.js
+++ transporte/src/routes/routes.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import login from '../components/login.vue';
 import buses from '../components/buses.vue';
 import menu from '../components/menu.vue';
+import cliente from '../components/cliente.vue';
 
 const routes = [
     { path: '/', component: login },
     { path: '/menu', component: menu, children:[
-       { path: '/buses', component: buses } 
+       { path: '/buses', component: buses },
+       { path: '/cliente', component: cliente}
     ] },
     
 ];
